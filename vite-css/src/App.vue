b86e2a0da6ac222f6477c31a20b57955dd770780
HuangQingYu0727 <18680849027@163.com>
2025-04-24 23:58:22 +0800

    feat:vite-css

---
+++ vite-css/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+  // import "@styles/index.css"
+  import "@styles/test.less"
+  import classes from "@styles/test.module.css"
+</script>
+
+<template>
+  <div :class="[`root ${classes.moduleClass}`]">Hello Vite</div>
+</template>
+
+<style scoped>
+
+</style>
