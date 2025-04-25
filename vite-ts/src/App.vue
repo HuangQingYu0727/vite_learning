2bf9a0228a96a9a2b2c0b48a5af23927ccbbe127
HuangQingYu0727 <18680849027@163.com>
2025-04-25 22:09:08 +0800

    feat:vite-ts

---
+++ vite-ts/src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { a } from "./index.ts";
+</script>
+
+<template>
+  <div>Hello Ts -- {{ a.name }}</div>
+</template>
+
+<style scoped></style>
---
+++ vite-ts/src/index.ts
@@ -0,0 +1,8 @@
+interface A {
+    name: string;
+}
+
+export const a : A = {
+    name: "HHH",
+    age: 20
+}
